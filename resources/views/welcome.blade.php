cf3baf76035a3e891c871dcef081f05b5fe5db3f
Pranto Biswas <prantobd320@gmail.com>
2025-12-11 15:46:06 +0600

    update : blah2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -206,12 +206,41 @@
                             </p>
 
                             <h2 class="font-bold text-lg text-sky-800/70">
-                                <del>$70</del> <span class="text-gray-800">$60</span>
+                                <del>$70</del> <span class="text-gray-800">$30</span>
                             </h2>
                         </div>
                     </div>
 
                     <a href="{{ route('product_view', ['slug' => 'phone-call-reading']) }}">
+                        <button
+                            class="mt-4 bg-gray-900 font-extrabold py-2 px-4 rounded-lg text-yellow-300 hover:bg-gray-800 transition-colors w-full">
+                            Get Your Reading Now
+                        </button>
+                    </a>
+
+                </div>
+
+                {{-- Astro Human Design call --}}
+                <div
+                    class="reading_item bg-white border border-black rounded-lg shadow-lg p-4 flex flex-col hover:scale-105 transition-transform">
+
+                    <div class="flex">
+                        <img src="{{ asset('images/ph_product.png') }}" alt="Product Image"
+                            class="min-w-32 min-h-32 max-w-32 max-h-32 mr-3 object-cover bg-gray-200 rounded-lg">
+
+                        <div>
+                            <h3 class="text-xl font-semibold mb-1">Human Design Chart</h3>
+                            <p class="text-sky-600 drop-shadow text-sm font-semibold mb-2">
+                                Hop on a 1on1 call with me and ask me anything astrology related!
+                            </p>
+
+                            <h2 class="font-bold text-lg text-sky-800/70">
+                                <del>$70</del> <span class="text-gray-800">$30</span>
+                            </h2>
+                        </div>
+                    </div>
+
+                    <a href="{{ route('product_view', ['slug' => 'human-design-chart-reading']) }}">
                         <button
                             class="mt-4 bg-gray-900 font-extrabold py-2 px-4 rounded-lg text-yellow-300 hover:bg-gray-800 transition-colors w-full">
                             Get Your Reading Now
