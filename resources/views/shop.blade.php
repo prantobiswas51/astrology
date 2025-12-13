42bf15aa0f505c547e6a77ff74b18bc8e4ef47cb
Pranto Biswas <prantobd320@gmail.com>
2025-12-13 11:35:37 +0600

    update : major update

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -3,7 +3,7 @@
     <div class="container mx-auto px-4 py-8">
         <h1 class="text-3xl font-bold mb-6">Shop</h1>
         <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
-            @foreach($products as $product)
+            @foreach($pub_products as $product)
                 <div class="border rounded-lg p-4 shadow bg-sky-100">
                     <img src="{{ asset('storage/' . $product->image1_path) }}" alt="{{ $product->name }}" class="w-full h-auto max-h-[260px] object-cover mb-4">
                     <h2 class="text-xl font-semibold mb-2">{{ $product->name }}</h2>
